d283e8244e7ca7e23c720394ce0c67da8fed9d1f
Joswin Francis <joswinfrancis2001@gmail.com>
2024-04-23 15:17:36 +0530

    category path edited

--- storybookapp/urls.py
+++ storybookapp/urls.py
@@ -5,7 +5,7 @@ app_name = 'storybookapp'
 
 urlpatterns = [
     path('', views.story_list,name="story_list"),
-    path('/<slug:category_slug>', views.story_list,name="story_category"),
+    path('<slug:category_slug>', views.story_list,name="story_category"),
     path('<int:id>/', views.story_detail,name="story_detail"),  
     path('search/', views.search_story,name="story_search")
 ] 
